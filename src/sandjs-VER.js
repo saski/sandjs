b6e70ea612c466cdb7e536a6f57fce1ceab4641d
Ignacio Sánchez <chet161984@gmail.com>
2012-12-08 09:26:18 +0100

    Added the latest builded file... maybe this file needs to be in another directory...

--- src/sandjs-VER.js
+++ src/sandjs-VER.js
@@ -1,12 +1,16 @@
-var debug = {
-	stat : "Debug"
-}/*!
+/*
  * 
- * Date: @DATE
- */
+ *
 (function( window, undefined ) {
 "use strict";
-var ajax = {
-	status = "Ajax"
+*/
+var debug = {
+	status : "Debug",
+	version : "1.2"
 }
+var ajax = {
+	status : "Ajax"
+}
+/*
 })( window );
+*/
